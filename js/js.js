40c993bc07936e9c5f78efd997c890b53b2c3133
Ramon Sousa <ramon.sousa1997Woutlook.com>
2022-06-13 21:26:44 -0300

    s

--- js/js.js
+++ js/js.js
@@ -2,12 +2,39 @@
     $(document).on("scroll", function() {
       if ($(window).scrollTop() > 100) {
         console.log("hide");
-        $('nav').addClass('white')
+        $('nav').addClass('white');
+        document.getElementById('list').style.backgroundColor = "rgba(67, 82, 120, 0.8)";
       } else {
         console.log("show");
-        $('nav').removeClass('white')      
+        $('nav').removeClass('white');
+        document.getElementById('list').style.backgroundColor = "rgba(225, 225, 225, 0.2)"; 
       }
     });
+  });
+
+  $(document).ready(function(){
+    const phrases = [
+
+      '',
+      'ATUALIZAÇÕES PERIÓDICAS E DE RELEASE',
+      'CUSTOMIZAÇÃO',
+      'IMPLANTAÇÃO',
+      'TREINAMENTO'
+    ];
+    let index = 0;
+    setInterval(function () {
+      if(index == 4){
+        index = 0;
+      }
+      $("#text").fadeOut(1000);
+      setTimeout(function(){
+        
+      document.getElementById('text').innerHTML = phrases[index];
+      },1000);
+      $("#text").fadeIn(1000);
+      index ++;
+
+    }, 3000);
   });
 
   $("inicio").click(function() {
@@ -16,77 +43,4 @@
         'slow');
 });
 
-  class TextScramble {
-    constructor(el) {
-      this.el = el;
-      this.chars = '!<>-_\\/[]{}—=+*^?#________';
-      this.update = this.update.bind(this);
-    }
-  
-    setText(newText) {
-      const oldText = this.el.innerText;
-      const length = Math.max(oldText.length, newText.length);
-      const promise = new Promise(resolve => this.resolve = resolve);
-  
-      this.queue = [];
-      for (let i = 0; i < length; i++) {
-        const from = oldText[i] || '';
-        const to = newText[i] || '';
-        const start = Math.floor(Math.random() * 40);
-        const end = Math.floor(Math.random() * 40) + start;
-        this.queue.push({ from, to, start, end });
-      }
-  
-      cancelAnimationFrame(this.frameRequest);
-      this.frame = 0;
-      this.update();
-      return promise;
-    }
-  
-    update() {
-      let output = '';
-      let complete = 0;
-      for (let i = 0, n = this.queue.length; i < n; i++) {
-        let { from, to, start, end, char } = this.queue[i];
-  
-        if (this.frame >= end) {
-          complete++;
-          output += to;
-        } else if (this.frame >= start) {
-          if (!char || Math.random() < 0.28) {char = this.randomChar();this.queue[i].char = char;}
-          output += `<span class='dud'>${char}</span>`;
-        } else {
-          output += from;
-        }
-      }
-      this.el.innerHTML = output;
-      if (complete === this.queue.length) {this.resolve();} else {
-        this.frameRequest = requestAnimationFrame(this.update);this.frame++;}
-    }
-  
-    randomChar() {
-      return this.chars[Math.floor(Math.random() * this.chars.length)];
-    }}
-  
-  
-  const phrases = [
-  'ATUALIZAÇÕES PERIÓDICAS E DE RELEASE',
-  'CUSTOMIZAÇÃO',
-  'IMPLANTAÇÃO',
-  'TREINAMENTO'
-];
-  
-  
-  const el = document.querySelector('.text');
-  const fx = new TextScramble(el);
-  
-  let counter = 0;
-  const next = () => {
-    fx.setText(phrases[counter]).then(() => {
-      setTimeout(next, 800);
-    });
-    counter = (counter + 1) % phrases.length;
-  };
-  
-  next();
   
